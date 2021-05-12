ce864178735862828bde69b5a21fdf2381c31482
antocrne <69148589+antocrne@users.noreply.github.com>
2021-05-12 11:54:24 +0200

    web update

--- js/script.js
+++ js/script.js
@@ -1,59 +1,3 @@
-$(document).ready(function(){
-
-    
-    // Preload images
-    const preloadImages = () => {
-            
-        return new Promise((resolve, reject) => {
-            imagesLoaded(document.querySelectorAll('img'), resolve);
-            $('body').addClass('stop-scrolling');
-            $('body').bind('touchmove', function(e){e.preventDefault()});
-
-        });
-
-        
-    };
-    
-    // And then..
-    preloadImages().then(() => {
-        // Remove the loader
-        //document.body.classList.remove('loading');
-        var el = document.getElementsByClassName( 'loading' );
-        $(el).remove();
-        $('body').removeClass('stop-scrolling');
-        $('body').unbind('touchmove');
-    });
-
-    
-});
-
-$(".toggle__menu").click(function(){
-    $('.menu__span').toggleClass('is-active');
-    $('.menu__overlay').toggleClass('menu-visible');
-    $('body').toggleClass('stop-scrolling');
-});
-
-$(document).ready(function (){
-    $("#mLink").click(function (){
-        $('html, body').animate({
-            scrollTop: $("#movies").offset().top
-        }, 1000);
-        $('.menu__overlay').removeClass('menu-visible');
-        $('.menu__span').removeClass('is-active');
-        $('body').removeClass('stop-scrolling');
-        $('body').unbind('touchmove');
-    });
-
-    $("#pLink").click(function (){
-        $('html, body').animate({
-            scrollTop: $("#photos").offset().top
-        }, 1000);
-        $('.menu__overlay').removeClass('menu-visible');
-        $('.menu__span').removeClass('is-active');
-        $('body').removeClass('stop-scrolling');
-        $('body').unbind('touchmove');
-    });
-});
 
 
 
@@ -76,40 +20,6 @@ $(function(){
 });
 
 
-
-$(".pc").click(function(){
-    var src = $(this).find('img').attr('src');
-    var img = $(this).find('img').hasClass('o__i');
-    $('body').addClass('stop-scrolling');
-    $('.photo__overlay').addClass('is-visible');
-    $(".overlay__img").append($('<img>',{class: 'o__img',src: src}));
-    $(".close__span").addClass('is-appear');
-    if (src = img) {
-        $(".overlay__img").addClass("portrait");
-    }
-
-    else {
-        $(".overlay__img").addClass("landscape"); 
-    }
-
-    $('.bg__po').addClass('bg-visible');
-    $('.o__content').addClass('is-appear');
-    $('body').bind('touchmove', function(e){e.preventDefault()});
-
-});
-
-$(".close").click(function(){
-    var src = $(this).find('img').attr('src');
-    $('.photo__overlay').removeClass('is-visible');
-    $(".o__img").remove(src);
-    $(".overlay__img").removeClass("portrait");
-    $(".overlay__img").removeClass("landscape");
-    $('.bg__po').removeClass('bg-visible');
-    $('.o__content').removeClass('is-appear');
-    $(".close__span").removeClass('is-appear');
-    $('body').removeClass('stop-scrolling');
-    $('body').unbind('touchmove');
-});
 
 
 $("#email").click(function() {
@@ -145,35 +55,3 @@ $("#email").click(function() {
     }, 1200);
 });
 
-$("#email2").click(function() {
-
-    if (document.selection) { // IE
-        var range = document.body.createTextRange();
-        range.moveToElementText(document.getElementById("email2"));
-        range.select();
-    } else if (window.getSelection) {
-        var range = document.createRange();
-        range.selectNode(document.getElementById("email2"));
-        window.getSelection().removeAllRanges();
-        window.getSelection().addRange(range);
-    }
-    document.execCommand("copy");
-
-    if (window.getSelection) {
-        window.getSelection().removeAllRanges();
-    }
-    else if (document.selection) {
-        document.selection.empty();
-    }
-
-    var posEmailX = $("#email2").offset().left;
-    var posEmailY = $("#email2").offset().top;
-    var emailHeight = $("#email2").height();
-    let emailContent = document.getElementById("email2").innerHTML;
-
-    document.getElementById("email2").innerHTML = "Email copied to clipboard";
-
-    setTimeout(function(){
-        document.getElementById("email2").innerHTML = "hello@antoinecorniere.com";
-    }, 1200);
-});
