16dbc40d6f5531576b0ba0be247bed6c537d3b3f
antocrne <69148589+antocrne@users.noreply.github.com>
2025-03-19 15:35:08 +0100

    Test Update Scroll Working

--- test/assets/js/script.js
+++ test/assets/js/script.js
@@ -1,9 +1,10 @@
 gsap.registerPlugin(ScrollTrigger);
 
+// Sélectionne le container du carousel
 const container = document.querySelector(".carousel");
 const slides = document.querySelectorAll(".slide");
-const totalWidth = container.scrollWidth;
-const totalHeight = container.scrollHeight;
+const totalHeight = container.scrollHeight; // Hauteur totale du carousel
+const totalWidth = container.scrollWidth;   // Largeur totale du carousel
 
 // Vérifie si l'écran est de type mobile
 const isMobile = window.innerWidth <= 768;
@@ -11,12 +12,12 @@ const isMobile = window.innerWidth <= 768;
 if (isMobile) {
   // Animation verticale pour les écrans mobiles
   gsap.to(container, {
-    y: () => -totalHeight + window.innerHeight,
+    y: () => -totalHeight + window.innerHeight, // Déplace le container sur toute la hauteur
     ease: "none",
     scrollTrigger: {
       trigger: ".scroll-container",
       start: "top top",
-      end: () => `+=${totalHeight}`,
+      end: () => `+=${totalHeight}`, // L'animation se termine à la fin de la hauteur du contenu
       scrub: 1,
       pin: true,
     },
@@ -24,7 +25,7 @@ if (isMobile) {
 } else {
   // Animation horizontale pour les écrans plus larges
   gsap.to(container, {
-    x: () => -totalWidth + window.innerWidth,
+    x: () => -totalWidth + window.innerWidth, 
     ease: "none",
     scrollTrigger: {
       trigger: ".scroll-container",
@@ -48,8 +49,19 @@ slides.forEach((slide) => {
       scrub: 1.2,
     },
   });
-});
 
+  // Effet parallaxe sur le titre des projets
+  gsap.to(slide.querySelector(".project-title-hidden"), {
+    y: 30, // Décalage vertical du titre
+    opacity: 1, // Appliquer l'opacité pour un effet d'apparition
+    scrollTrigger: {
+      trigger: slide,
+      start: isMobile ? "top bottom" : "left center",
+      end: isMobile ? "bottom top" : "right center",
+      scrub: 1.2,
+    },
+  });
+});
 
 // Sélectionne les éléments de l'image dans le carousel pour appliquer l'effet hover
 slides.forEach((slide) => {
